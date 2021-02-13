41e14d11dab361883ffa7ae69d9d1cacca2c44a3
Ansh651 <72167893+Ansh651@users.noreply.github.com>
2021-02-13 10:16:34 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -17,16 +17,52 @@ function take_snapshot(){
 }
 
 console.log('ml5version:', ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Ur_QrTS8H/model.json',modelLoaded);
+classifier = ml5.imageClassifier('https://storage.googleapis.com/tm-model/5mxzZHkpx/model.json',modelLoaded);
 
 function modelLoaded(){
     console.log("Model Loaded");
 }
 
+function check() {
+    img = document.getElementById("captured_image");
+    classifier.classify(img, gotResults);
+}
 function Speak(){
     var synth = window.speechSynthesis;
     speak_data_1="The first prediction is " + prediction_1;
     speak_data_2="And the second prediction is " + prediction_2;
     var utterThis=new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
     synth.speak(utterThis);
+}
+
+function gotResults(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    else {
+         console.log(results);
+         document.getElementById("result_emotion_name").innerHTML = results[0].label;
+         document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+         prediction_1 = results[0].label;
+         prediction_2 = results[1].label;
+         Speak();
+         if(results[0].label == "amazing") {
+             document.getElementById("update_emoji").innerHTML = "&#x1F44C;";
+         }
+         if(results[0].label == "best") {
+            document.getElementById("update_emoji").innerHTML = "&#x1F44D;";
+        }
+        if(results[0].label == "victory") {
+            document.getElementById("update_emoji").innerHTML = "&#x270C;";
+        }
+        if(results[1].label == "amazing") {
+            document.getElementById("update_emoji2").innerHTML = "&#x1F44C;";
+        }
+        if(results[1].label == "best") {
+            document.getElementById("update_emoji2").innerHTML = "&#x1F44D;";
+        }
+        if(results[1].label == "victory") {
+            document.getElementById("update_emoji2").innerHTML = "&#x270C;";
+        }
+    }
 }
